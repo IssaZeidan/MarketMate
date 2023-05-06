49424a95f34c188aa44b85d7f24f45f1a00e0fea
saad-zireeni <saad.alzireeni@gmail.com>
2023-05-06 16:08:49 +0300

    files org2

--- market-mate/src/App.js
+++ market-mate/src/App.js
@@ -4,17 +4,23 @@ import Products from './pages/Products';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Landing/>} />
           <Route path="Products" element={<Products/>} />
           <Route path="About" element={<About/>} />
           <Route path="Contact" element={<Contact/>} />
+          <Route path="Login" element={<Login/>} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
