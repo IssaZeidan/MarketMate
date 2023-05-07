45c8751a4dea576d69f5a5c24bb45c923382dca3
saad-zireeni <saad.alzireeni@gmail.com>
2023-05-07 15:33:41 +0300

    search text color edited

--- market-mate/src/components/P-Categories/Search.js
+++ market-mate/src/components/P-Categories/Search.js
@@ -48,7 +48,7 @@ const Search = () => {
             type="text"
             id="simple-search"
             onChange={handleSearch}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-white"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-white text-black"
             placeholder="Search"
             required
           />
