4a1cae965ca47e52248841c97301156c82bcfd6b
saad-zireeni <saad.alzireeni@gmail.com>
2023-05-08 10:05:12 +0300

    removed unused function

--- market-mate/src/components/P-Categories/All.js
+++ market-mate/src/components/P-Categories/All.js
@@ -6,9 +6,7 @@ const All = () => {
 
   const { data } = useContext(ProductContext);
 
-  function handleClick(name, price){
-    console.log(name + " " + price);
-  }
+  
 
   return (
     <>
@@ -38,7 +36,6 @@ const All = () => {
             <a
               href="#"
               className="inline-block px-2 py-1 text-xs font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              onClick={() => handleClick(d.name, d.price)}
             >
               Add to Cart
             </a>
